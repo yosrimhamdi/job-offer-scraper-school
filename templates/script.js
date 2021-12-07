e3b250c7c34026b47996d55ad807e55054d2f6d9
Rimao416 <omarkayumba12345@gmail.com>
2021-12-07 09:35:50 +0100

    Page2

---
+++ templates/script.js
@@ -0,0 +1,23 @@
+//document.querySelector("link[href='style.css']").href = ".css";
+$('.one').click(function(){
+    if($(this).hasClass('active')){
+        return;
+    }else{
+        $(this).addClass('active')
+        $('.two').removeClass('active')
+        $('link[href="style2.css"]').attr('href','style.css');
+    }
+})
+$('.two').click(function(){
+    if($(this).hasClass('active')){
+        return;
+    }else{
+        $(this).addClass('active')
+        $('.one').removeClass('active')
+        $('link[href="style.css"]').attr('href','style2.css');
+    }
+})
+$(document).on('mouseover','.land',function(){
+    let phrase=$(this).attr('title')
+    $('#Gouvernorat').text(phrase)
+ })
