48e035c907ef2aade1927b9d15aa10f6e848a7ad
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-30 22:09:37 +0100

    Filled the Footer component with the appropriate markup.

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer class="modif-footer text-light position-absolute mt-3 mb-0 w-100">
+      <div class="container">
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-light">
+                Contact
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-light">
+                Privacy policy
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-light">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-light">
+                FAQs
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-light">
+                About
+              </a>
+            </li>
+          </ul>
+          <p class="text-center">© 2021 Université Internationale de Tunis</p>
+        </footer>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
