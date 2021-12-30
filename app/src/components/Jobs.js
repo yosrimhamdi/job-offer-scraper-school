9e9a25ddadaab723c7e806a19819a889b2fbf6a8
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-30 22:08:31 +0100

    Filled the Jobs component with the appropriate markup.

--- app/src/components/Jobs.js
+++ app/src/components/Jobs.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const Jobs = () => {
-  return <div>jobs</div>;
+  return (
+    <section className="container-fluid row justify-content-between my-5">
+      <div className="card my-3" style={{ width: '18rem' }}>
+        <img
+          src="/images/company-logo.png"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+        <div className="btn-container">
+          <a href="#" className="card-link">
+            Card link
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Jobs;
