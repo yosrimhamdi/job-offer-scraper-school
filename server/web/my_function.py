fc56a82c5c9e574532eb64bdfd18ad8b8a163f12
Banitongwa7 <davidbanitongwa2@yahoo.com>
2022-01-09 19:01:39 +0100

    scraper enterprise postTime and link

--- server/web/my_function.py
+++ server/web/my_function.py
@@ -3,6 +3,7 @@ from bs4.element import SoupStrainer
 import requests
 
 
+
 def my_scraping(mon_url,tag,number,adresse,class_adresse,d_class='',p_class=''): 
     i=0
     j=1
@@ -11,7 +12,6 @@ def my_scraping(mon_url,tag,number,adresse,class_adresse,d_class='',p_class=''):
     while(j<number):
         mes_donnees=(phrase(mon_url,str(j)))
         j+=1
-        company_name=''
         html_text=requests.get(mes_donnees).text
         #html_text=requests.get('https://www.optioncarriere.tn/emplois-tunisie-123097.html?p=1').text
         #html_text=requests.get('https://www.jobi.tn/#!/?q=page=9&sortBy=updated_at&sortOrder=desc').text
@@ -23,11 +23,18 @@ def my_scraping(mon_url,tag,number,adresse,class_adresse,d_class='',p_class=''):
         #jobs=soup.find_all('div',class_="hpanel m-none m-b-xs joboffer-preview font-8 m-b-sm")
         for job in jobs:
             
-            company_name =job.find(p_class).text
+            jobTitle =job.find(p_class).text
+            enterprise = job.find('p',class_='company').text
+            postTime = job.find('span',class_='badge-icon').text
 #            company_addresse=job.find(adresse,'span12 no-margin-left').text
-            company_addresse=job.find(adresse,class_=class_adresse).text
-            ma_list.append({'emploi':company_name.strip(),'adresse':company_addresse.strip()})
+            jobAdresse =job.find(adresse,class_=class_adresse).text
+
+            linkAll = job.find('a')
             
+            link = 'https://www.optioncarriere.tn'+str(linkAll.get('href'))
+
+            ma_list.append({'emploi':jobTitle.strip(),'enterprise': enterprise.strip(),'adresse':jobAdresse.strip(), 'postTime': postTime.strip(), 'lienPost': link})
+
             #database.update(ma_list)
         
             #skills=job.find('span',class_='srp-skills').text.replace(' ','')
@@ -43,6 +50,7 @@ def phrase(a,b):
     return str(a)+(b)
 def remove_space(a):
     return a.lstrip().rstrip()
+
 
 def concat_list():
 #    post1=my_scraping('https://www.keejob.com/offres-emploi/?page=','div',2,'div','block_white_a post clearfix silver-job-block','h6')
