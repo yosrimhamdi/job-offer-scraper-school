6629f4432dcd721abf847f21ac34e16b0f2ae223
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-13 23:59:20 +0100

    Created the home amd the searh result page with its app routing.

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import SearchResult from './SearchResult';
+import Home from './Home';
 
 const App = () => {
-  return <div>App </div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:term" element={<SearchResult />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
