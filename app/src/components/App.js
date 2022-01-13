dff102d08ed6be663077480ef0a5ab508dc02d2f
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-13 23:39:06 +0100

    Restored the default react app.

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import Header from './Header';
-import Search from './Search';
-import Jobs from './Jobs/Jobs';
 import Footer from './Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Search />
-      <Jobs />
       <Footer />
     </div>
   );
--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,44 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer className="modif-footer text-light position-absolute mt-3 mb-0 w-100">
-      <div className="container">
-        <footer className="py-3 my-4">
-          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-            <li className="nav-item">
-              <a href="#" className="nav-link px-2 text-light">
-                Contact
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link px-2 text-light">
-                Privacy policy
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link px-2 text-light">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link px-2 text-light">
-                FAQs
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link px-2 text-light">
-                About
-              </a>
-            </li>
-          </ul>
-          <p className="text-center">
-            © 2021 Université Internationale de Tunis
-          </p>
-        </footer>
-      </div>
-    </footer>
-  );
+  return <div>Footer</div>;
 };
 
 export default Footer;
