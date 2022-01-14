7ec18a01f2c5e9eb75dc59428bc51d0b49fa26d3
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-14 12:01:16 +0100

    Displaying the footer component.

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchResult from './SearchResult';
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/search/:term" component={SearchResult} />
       </Switch>
+      <Footer />
     </Router>
   );
 };
