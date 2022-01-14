553a91c21cf22fdebf21cc105e5340f62646a11f
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-14 11:58:03 +0100

    Removed the Spinner component.

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -4,12 +4,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchResult from './SearchResult';
 import Home from './Home';
 import Header from './Header';
-import Spinner from './Spinner';
 
 const App = () => {
   return (
     <Router>
-      <Spinner />
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
