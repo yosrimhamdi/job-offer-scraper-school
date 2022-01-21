c983be74b59e85970d868f9e0626bbd249ed5f4b
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-21 10:50:34 +0100

    Dynamic server URL based on react app url.

--- app/src/constants/SERVER_URL.js
+++ app/src/constants/SERVER_URL.js
@@ -1,1 +1,9 @@
-export const SERVER_URL = 'http://127.0.0.1:8000/';
+function getServerURL() {
+  if (window.location.href.includes('netlify.app')) {
+    return 'https://react-django-res-api.herokuapp.com/';
+  }
+
+  return 'http://127.0.0.1:8000/';
+}
+
+export const SERVER_URL = getServerURL();
