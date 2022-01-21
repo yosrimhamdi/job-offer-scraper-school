b7f55b35c007c9a55ad568b69b0faff10bf0c2ed
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-21 10:59:05 +0100

    Changed setting.py.

--- server/webscrapping/settings.py
+++ server/webscrapping/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'webscrapping.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / "db.sqlite3"),
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
